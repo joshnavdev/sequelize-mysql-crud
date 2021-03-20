9e4108f812828f58654ed9f3e3cd69c174bc2207
Joshua Navarro <joshua.navarro35@gmail.com>
2021-03-20 13:38:49 -0500

    Modify config file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-require('./src/config');
+const config = require('./src/config');
 const app = require('./src/app');
 
-const PORT = process.env.PORT;
+const PORT = config.app.port;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -4,3 +4,19 @@ const env = process.env.NODE_ENV || 'development';
 
 const dotenvFile = `${__dirname}/../../.env.${env}`;
 dotenv.config({ path: dotenvFile });
+
+const config = {
+  app: {
+    port: process.env.PORT || 8080,
+    env: process.env.NODE_ENV || 'development',
+  },
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+  },
+};
+
+module.exports = config;
